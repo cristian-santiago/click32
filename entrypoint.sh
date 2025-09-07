f6276457264f2e93224839e31bc798359063ff6f
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2025-09-07 01:34:06 -0300

    Refactor entrypoint script to use pg_isready for PostgreSQL readiness and add migration commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-set -e  # para parar se houver erro
 
 echo "Esperando o Postgres ficar pronto..."
-while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
-  sleep 1
-done
 
-echo "Banco pronto! Aplicando migrations..."
-python manage.py makemigrations
-python manage.py migrate
+while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" > /dev/null 2>&1; do
+            sleep 1
+    done
+
+    echo "Banco pronto! Aplicando migrations..."
+
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py runserver 0.0.0.0:8000
 
 exec "$@"  # executa o comando passado no docker-compose
